dc4db454568d5e2f1813dca68749cbf54569ca71
Adolfo Panizo <adolfo.panizo@gmail.com>
2020-09-18 12:10:24 +0200

    Point to apt business report

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -7,7 +7,7 @@ if [ -e "$DIR" ]; then
   git fetch origin
   git checkout refs/tags/$VERSION
 else
-  git clone https://github.com/reg-viz/reg-cli-report-ui.git -b $VERSION $DIR --depth 1
+  git clone https://github.com/APT-Business/reg-cli-report-ui.git -b $VERSION $DIR --depth 1
   cd $DIR
 fi
 
